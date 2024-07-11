baeae7d3090795d8dbf55b7401a4d82e2743f5ec
Valentin Bojarski <valentin.boyarsk@gmail.com>
2024-07-11 21:29:48 +0300

    feature: add close and open sidebar functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { useLayoutEffect } from 'react';
-import { Outlet } from 'react-router-dom';
-import { refreshAccessToken } from './utils/refreshToken';
-import useAuthStore from './store/authStore';
-import './index.css';
+import { useLayoutEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { refreshAccessToken } from "./utils/refreshToken";
+import useAuthStore from "./store/authStore";
+import "./index.css";
 
 const App = () => {
-    const accessToken = useAuthStore((state) => state.accessToken);
+  const accessToken = useAuthStore((state) => state.accessToken);
 
-    useLayoutEffect(() => {
-        if (!accessToken) {
-            refreshAccessToken();
-        }
-    }, [accessToken]);
+  useLayoutEffect(() => {
+    if (!accessToken) {
+      refreshAccessToken();
+    }
+  }, [accessToken]);
 
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,15 +4,23 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import styles from "./Dashboard.module.css";
 import DatePicker from "./DatePicker";
+import useSideBarStore from "../store/sidebarStore";
 
 export default function Dashboard() {
   const location = useLocation();
   const dashboardRoute = location.pathname === "/dashboard";
+  const { sidebarIsOpen } = useSideBarStore();
 
   return (
     <div className={styles.dashboard}>
       <Sidebar />
-      <div className={styles.home_container}>
+      <div
+        className={
+          sidebarIsOpen
+            ? styles.home_container
+            : `${styles.home_container} ${styles.container_max}`
+        }
+      >
         {dashboardRoute && (
           <>
             <Header sectionName={"Dashboard"} />
