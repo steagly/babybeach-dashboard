cc92732079d9babe017c2d3ccf5c925be01fc468
Valentin Bojarski <valentin.boyarsk@gmail.com>
2024-08-04 00:15:31 +0300

    feat: add booking summary config

--- src/store/bookingStore.js
+++ src/store/bookingStore.js
@@ -1,43 +1,80 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { bookingServices } from "../configs/booking/booking";
 
-const useBookingStore = create((set) => ({
-  selectedDate: new Date(),
-  selectedTimeSlot: {},
-  timeSlots: [],
-  bookingInfo: {
-    date: "",
-    hour: 13,
-    lastName: "",
-    firstName: "",
-    adult: 0,
-    baby: 0,
-    kid: 0,
-    phone: "",
-    email: "",
-    card: 12,
-  },
-
-  changeDate: (date) => set({ selectedDate: date }),
-
-  setTimeSlots: (timeSlots) => set({ timeSlots: timeSlots }),
-
-  setSelectedTimeSlot: (timeSlot) => set({ selectedTimeSlot: timeSlot }),
-
-  changePersonCount: (name, action) =>
-    set((state) => ({
-      bookingInfo: {
-        ...state.bookingInfo,
-        [name]: Math.max(
-          0,
-          Math.min(
-            9,
-            action === "increase"
-              ? state.bookingInfo[name] + 1
-              : state.bookingInfo[name] - 1
-          )
-        ),
+const useBookingStore = create(
+  devtools((set) => ({
+    selectedDate: new Date(),
+    selectedTimeSlot: {},
+    timeSlots: [],
+    services: bookingServices,
+    bookingInfo: {
+      date: "",
+      lastName: "",
+      firstName: "",
+      phone: "",
+      email: "",
+      card: 12,
+      adult: {
+        count: 0,
+        price: 14.5,
       },
-    })),
-}));
+      baby: {
+        count: 0,
+        price: 0,
+      },
+      kid: {
+        count: 0,
+        price: 2.5,
+      },
+      teenager: {
+        count: 0,
+        price: 7.5,
+      },
+    },
+
+    changeDate: (date) => set({ selectedDate: date }),
+
+    setTimeSlots: (timeSlots) => set({ timeSlots: timeSlots }),
+
+    setSelectedTimeSlot: (timeSlot) =>
+      set((state) => ({
+        selectedTimeSlot: timeSlot,
+        bookingInfo: { ...state.bookingInfo, date: "" },
+      })),
+
+    changePersonCount: (name, action) =>
+      set((state) => ({
+        bookingInfo: {
+          ...state.bookingInfo,
+          [name]: {
+            ...state.bookingInfo[name],
+            count: Math.max(
+              0,
+              Math.min(
+                9,
+                action === "increase"
+                  ? state.bookingInfo[name].count + 1
+                  : state.bookingInfo[name].count - 1
+              )
+            ),
+          },
+        },
+      })),
+
+    setService: (service) => {
+      set((state) => {
+        return {
+          ...state,
+          services: state.services.map((currService) =>
+            service.id === currService.id
+              ? { ...currService, count: currService.count + 1 }
+              : currService
+          ),
+        };
+      });
+    },
+  }))
+);
 
 export default useBookingStore;
